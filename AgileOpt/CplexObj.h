0119ae7b5f0c6e8846c955ef7b7235f11b83d0dc
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-11-10 10:51:49 +0100

    add original file

---
+++ AgileOpt/CplexObj.h
@@ -0,0 +1,161 @@
+//-----------------------------------------------------------------------------
+//  File: CplexObj.h                                                       
+//                                                                           
+//  Project: Cplex solver interface (header)                   
+//                                                                           
+//  Author: M.A. Boschetti                                                   
+//                                                                           
+//  Last update: 24.08.2011                                                  
+//-----------------------------------------------------------------------------
+//
+#include "cplex.h"
+#include "knapsack.h"
+
+struct tupla {
+	long j1,j2,j3;
+};
+
+struct cutinfo {
+	int FlagLP;   // FlagLP=0 if we are solving the original model; FlagLP=1 if we are solving the knapsack subproblem; 
+	CPXENVptr env;  // Pointer to Cplex Environment 
+	CPXLPptr lp;
+	CPXLPptr lpkp;
+	int      numcols;
+	int      numtoadd;
+	int      maxcuts;
+	int      num;
+	int      nz;
+	double   *x;
+	int      *beg;
+	int      *ind; 
+	double   *val;
+	double   *rhs;
+	int      *Flag2I;
+	int      *Flag3I;
+	struct tupla **Tupla2I;
+	struct tupla **Tupla3I;
+	int      *Flag2IRev;
+	int      *Flag3IRev;
+	struct tupla **Tupla2IRev;
+	struct tupla **Tupla3IRev;
+	long n,m;
+	double *u;    // u[j]*rcr[j] is the "base" weighted utility of story j
+	double *p;    // p[j]*run[j] is the number of story points of story j
+	double *pmax; // pmax[i] is the capacity of sprint i, measured in story points
+	long *nY;     // nY[j] is the number of stories similar to story j
+	long **Y;     // Y[j][k] is the k-th story similar to story j
+	long *nUOR;   // nUOR[j] is the number of stories having an OR dependency with story j
+	long **UOR;   // UOR[j][k] is the k-th story having an OR dependency with story j
+	long *nUAND;  // nUAND[j] is the number of stories having an AND dependency with story j
+	long **UAND;  // UAND[j][k] is the k-th story having an AND dependency with story j
+	long *FDepA;  // FDepA[j] is 1 if the story j is involved in some dependency (Active)
+	long *FDepP;  // FDepP[j] is 1 if the story j is involved in some dependency (Passive)
+	long *Cuts;
+	int Pred;     // Does it apply precedence inequalities?
+	int Cover;    // Does it apply cover inequalities?
+	int Lifting;  // Does it apply lifting?
+	int KnapSol;  // How solve the KP?
+	Knapsack KP;
+};
+
+typedef struct cutinfo CUTINFO, *CUTINFOptr;
+
+// Function implementing a custom cutting plane procedure called by callback
+static int CPXPUBLIC mycutcallback (CPXCENVptr env, void *cbdata, int wherefrom,
+                void *cbhandle, int *useraction_p);
+static int CPXPUBLIC myNewcutcallback (CPXCENVptr env, void *cbdata, int wherefrom,
+                void *cbhandle, int *useraction_p);
+static int CPXPUBLIC myHeucutcallback (CPXCENVptr env, void *cbdata, int wherefrom,
+                void *cbhandle, int *useraction_p);
+int OR_AND_Inequalities(CUTINFOptr cutinfo);
+int OR_AND_InequalitiesHeu(CUTINFOptr cutinfo);
+int Cover_Inequalities(CUTINFOptr cutinfo);
+int Cover_InequalitiesCplex(CUTINFOptr cutinfo);
+int Precedence_Inequalities(CUTINFOptr cutinfo);
+int Precedence_Inequalities_2Item(CUTINFOptr cutinfo);
+int Precedence_Inequalities_2ItemRev(CUTINFOptr cutinfo);
+int Precedence_Inequalities_3Item(CUTINFOptr cutinfo);
+int Precedence_Inequalities_3ItemRev(CUTINFOptr cutinfo);
+int CalcolaFlag2I(CUTINFOptr cutinfo);
+int CalcolaFlag3I(CUTINFOptr cutinfo);
+int CalcolaFlag2IRev(CUTINFOptr cutinfo);
+int CalcolaFlag3IRev(CUTINFOptr cutinfo);
+
+class CplexObj
+{
+private:
+
+	int status;
+
+	int CplexObj::SetupKPDP(CUTINFOptr cutinfo);
+	int CplexObj::SetupKPMIP(CUTINFOptr cutinfo);
+
+public:
+
+	CPXENVptr env;  // Pointer to Cplex Environment 
+	CPXLPptr lp;    // Pointer to LP instance
+	CUTINFO cutinfo;  // Data structure used to add cutting planes
+	
+	// Input
+	int minmax;     // Mininum or Maximum (1=minimize; -1=maximize) 
+	int ncols;      // Number of columns/variables
+	int nrows;      // Number of rows/constraints
+	int nz;         // Number of non-zero coefficients into the constraint matrix
+	int objsen;     // Minimization/Maximization Flag (+1=Min; -1=Max)
+	double *obj;    // Objective Function coefficients
+	double *rhs;    // Right Hand Side
+	char *sense;    // Constraint Senses
+	int *matbeg;    // Pointers to the beginning of each columns of the constraint matrix
+	int *matcnt;    // Cardinality of each columns of the constraint matrix
+	int *matind;    // Row index associated to each coefficient of the constraint matrix
+	double *matval; // Value of each coefficient of the constraint matrix
+	double *lb;     // Lower Bound value of each variable
+	double *ub;     // Upper Bound value of each variable
+	int *indices;
+	int *priority;
+	int *direction;
+	char *xctype;
+	double *rngval;
+
+	int nsos;       // Number of SOS (Special Ordered Set) 
+	int nsosnz;     // Total number of membres in all SOS added
+	char *typesos;  // Array containing SOS type (CPX_TYPE_SOS1='1', CPX_TYPE_SOS2='2')
+	int *sosbeg;    // Pointers to the beginning of each SOS
+	int *sosind;    // Index associated to the SOS member
+	double *soswt;  // Weight associated to the SOS member
+
+	// Output
+	double *xt;     // Variable Values
+	double objval;  // Objective Function Value
+
+	// Constructor and Destructor
+	CplexObj(void);
+	~CplexObj(void);
+
+	// MallocCols: Allocate the "column" data structure
+	int CplexObj::MallocCols(void);
+
+	// CopyLP
+	int CplexObj::CopyLP(void);
+
+	// Set a Lower Bound for the MIP
+	int CplexObj::SetLB(double LB);
+
+	// SetMIP
+	int CplexObj::SetMIP(double TLim);
+
+	// Sentinel Constraint
+	int CplexObj::Sentinel(long n, long m);
+
+	// SolveMIP
+	int CplexObj::SolveMIP(double *Zopt, double *Gap, long *Nodes, long *Cuts);
+
+    // Setup Custom Cutting Plane
+	int CplexObj::CuttingPlane(long n, long m, double* u, double* p, double* pmax, 
+		                       long *nY, long **Y, long *nUOR, long **UOR, long *nUAND, long **UAND, long *FDepA, long *FDepP, 
+							   int Pred, int Cover, int Lifting, int KnapSol, int MaxCuts, long *Cuts);
+	int CplexObj::CuttingPlaneHeu(long n, long m, double* u, double* p, double* pmax, 
+  	                          long *nY, long **Y, long *nUOR, long **UOR, long *nUAND, long **UAND, long *FDepA, long *FDepP,
+	                          int Pred, int Cover, int Lifting, int KnapSol, int MaxCuts, long *Cuts, long fact);
+
+};
---
+++ main.cpp
@@ -0,0 +1,322 @@
+//-----------------------------------------------------------------------------
+//  File: main.cpp                                                           
+//                                                                           
+//  Project: Optimize the Agile Project Management Schedule                  
+//                                                                           
+//  Author: M.A. Boschetti                                                   
+//                                                                           
+//  Last update: 28.08.2012                                                  
+//-----------------------------------------------------------------------------
+//
+#include <stdio.h>
+#include <time.h>
+#include "AgileOpt\AgileOpt.h"
+
+long MangiaPath(char *fname);
+
+void main(void)
+{
+	FILE *fpro;
+	FILE *fout;
+	FILE *fout1;
+	FILE *fout2;
+	AgileOpt Prob;
+	
+	long npro;
+	long i,j;
+	long kount1,kount2,kount3;
+	long t1,t2;
+	double dt;
+	double zheu,gap,zlp;
+	char fname[255];
+	double TLimMIP;
+	double TLimMIPb;
+	double TLimHEU;
+
+	fpro = fopen("..\\Agile.pro","r");
+	fout = fopen("..\\Agile-LP.out","w");
+	fout1 = fopen("..\\Agile-Heu.out","w");
+	fout2 = fopen("..\\Agile-Pro.out","w");
+
+	// LP
+	fprintf(fout,"\n                              ");
+	fprintf(fout,"                   Cplex                 ");
+	fprintf(fout,"             Cplex + LagrHeu             ");
+	fprintf(fout,"                 Sentinel                ");
+	fprintf(fout,"                   Pred                  ");
+	fprintf(fout,"                   Cover                 ");
+	fprintf(fout,"               Lifted Cover            \n");
+
+	fprintf(fout,"Istanza                         ");
+	fprintf(fout,"  Zopt    Gap    Nodes   NCuts    Time   ");
+	fprintf(fout,"  Zopt    Gap    Nodes   NCuts    Time   ");
+	fprintf(fout,"  Zopt    Gap    Nodes   NCuts    Time   ");
+	fprintf(fout,"  Zopt    Gap    Nodes   NCuts    Time   ");
+	fprintf(fout,"  Zopt    Gap    Nodes   NCuts    Time   ");
+	fprintf(fout,"  Zopt    Gap    Nodes   NCuts    Time   \n");
+	fflush(fout);
+
+	// Heuristics
+	fprintf(fout1,"\n                              ");
+	fprintf(fout1,"                   Cplex                 ");
+	fprintf(fout1,"          Heuristic      ");
+	fprintf(fout1,"       Lagrangean Heuristic      ");
+	fprintf(fout1,"   Lagrangean Heuristic (coef)   \n");
+
+	fprintf(fout1,"Istanza                         ");
+	fprintf(fout1,"  Zopt    Gap    Nodes   NCuts    Time   ");
+	fprintf(fout1,"  Zheu     Time   GHeu   ");
+	fprintf(fout1,"  Zheu     Time   Gap    GHeu   ");
+	fprintf(fout1,"  Zheu     Time   Gap    GHeu   \n");
+	fflush(fout1);
+
+	// Print instance characteristics
+	fprintf(fout2,"Istanza                         ");
+	fprintf(fout2,"    n       m      nY    |U^OR|  |U^AND|   \n");
+	fflush(fout2);
+	 
+	fscanf(fpro,"%d",&npro);
+
+	// Set Time Limit
+	Prob.cfg.TimeLimit=10.;
+	Prob.cfg.KnapSol=0;
+	Prob.cfg.MaxCuts=50000;
+	TLimMIP = 600.;
+	TLimMIPb = 600.;
+	TLimHEU = 600.;
+
+	for (i=0; i<npro; i++)
+	{
+		fscanf(fpro,"%s",fname);
+
+		Prob.ReadData(fname);
+		j=MangiaPath(fname);
+		fprintf(fout,"%-29s",fname+j);
+		fprintf(fout1,"%-29s",fname+j);
+		fprintf(fout2,"%-29s",fname+j);
+		fprintf(Prob.ferr,"\n%s:\n\n",fname);
+		fflush(Prob.ferr);
+
+		// Print instance characteristics
+		kount1=0;
+		kount2=0;
+		kount3=0;
+		for (j=0; j<Prob.n; j++)
+		{
+			if (Prob.nY[j]>1) kount1++;
+			if (Prob.nUOR[j]>0) kount2++;
+			if (Prob.nUAND[j]>0) kount3++;
+		}
+		fprintf(fout2,"%8d",Prob.n);
+		fprintf(fout2,"%8d",Prob.m);
+		fprintf(fout2,"%8d",kount1);
+		fprintf(fout2,"%8d",kount2);
+		fprintf(fout2,"%8d\n",kount3);
+		fflush(fout2);
+
+		// Cplex
+		Prob.cfg.TimeLimit=TLimMIP;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.Pred=0;
+		Prob.cfg.Cover=0;
+		Prob.cfg.Lifting=0;
+		Prob.cfg.AddLB=0;
+
+		t1 = clock();
+		Prob.Optimize();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+		zlp = Prob.Zopt;
+
+		fprintf(fout,"%10.1lf",Prob.Zopt);
+		fprintf(fout,"%7.2lf",Prob.Gap);
+		fprintf(fout,"%8d",Prob.Nodes);
+		fprintf(fout,"%8d",Prob.Cuts);
+		fprintf(fout,"%8.2lf",dt);
+		fflush(fout);
+
+		fprintf(fout1,"%10.1lf",Prob.Zopt);
+		fprintf(fout1,"%7.2lf",Prob.Gap);
+		fprintf(fout1,"%8d",Prob.Nodes);
+		fprintf(fout1,"%8d",Prob.Cuts);
+		fprintf(fout1,"%8.2lf",dt);
+		fflush(fout1);
+
+		// Heuristic procedure
+		Prob.cfg.TimeLimit=TLimHEU;
+		Prob.cfg.Sentinel=0;
+
+		t1 = clock();
+		Prob.OptimizeHeu();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout1,"%10.1lf",Prob.Zheu);
+		fprintf(fout1,"%8.2lf",dt);
+
+		gap = 100.*((zlp-Prob.Zheu)/zlp+0.00001);
+		fprintf(fout1,"%7.2lf",gap);
+		fflush(fout1);
+		zheu = Prob.Zheu;
+
+		// Lagrangian Heuristic procedure
+		Prob.cfg.TimeLimit=TLimHEU;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.HeuBest=1.0;
+
+		t1 = clock();
+		Prob.OptimizeLagrHeu();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout1,"%10.1lf",Prob.Zheu);
+		fprintf(fout1,"%8.2lf",dt);
+		gap = 100.*((Prob.Zlagr-Prob.Zheu)/Prob.Zheu);
+		fprintf(fout1,"%7.2lf",gap);
+
+		gap = 100.*((zlp-Prob.Zheu)/zlp+0.00001);
+		fprintf(fout1,"%7.2lf",gap);
+		fflush(fout1);
+		zheu = Prob.Zheu;
+
+		// Lagrangian Heuristic procedure with improvement even with HeuBest*Zub solution
+		Prob.cfg.TimeLimit=TLimHEU;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.HeuBest=0.995; // Best=0.995
+
+		t1 = clock();
+		Prob.OptimizeLagrHeu();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout1,"%10.1lf",Prob.Zheu);
+		fprintf(fout1,"%8.2lf",dt);
+		gap = 100.*((Prob.Zlagr-Prob.Zheu)/Prob.Zheu);
+		fprintf(fout1,"%7.2lf",gap);
+
+		gap = 100.*((zlp-Prob.Zheu)/zlp+0.00001);
+		fprintf(fout1,"%7.2lf\n",gap);
+		fflush(fout1);
+		zheu = Prob.Zheu;
+
+		// Cplex + LagrHeu
+		Prob.cfg.TimeLimit=TLimMIPb;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.Pred=0;
+		Prob.cfg.Cover=0;
+		Prob.cfg.Lifting=0;
+		Prob.cfg.AddLB=1;
+
+		t1 = clock();
+		Prob.Optimize();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout,"%10.1lf",Prob.Zopt);
+		fprintf(fout,"%7.2lf",Prob.Gap);
+		fprintf(fout,"%8d",Prob.Nodes);
+		fprintf(fout,"%8d",Prob.Cuts);
+		fprintf(fout,"%8.2lf",dt);
+		fflush(fout);
+
+		// Sentinel
+		Prob.cfg.TimeLimit=TLimMIPb;
+		Prob.cfg.Sentinel=1;
+		Prob.cfg.Pred=0;
+		Prob.cfg.Cover=0;
+		Prob.cfg.Lifting=0;
+		Prob.cfg.AddLB=0;
+
+		t1 = clock();
+		Prob.Optimize();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout,"%10.1lf",Prob.Zopt);
+		fprintf(fout,"%7.2lf",Prob.Gap);
+		fprintf(fout,"%8d",Prob.Nodes);
+		fprintf(fout,"%8d",Prob.Cuts);
+		fprintf(fout,"%8.2lf",dt);
+		fflush(fout);
+
+		// Pred
+		Prob.cfg.TimeLimit=TLimMIPb;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.Pred=1;
+		Prob.cfg.Cover=0;
+		Prob.cfg.Lifting=0;
+		Prob.cfg.AddLB=0;
+
+		t1 = clock();
+		Prob.Optimize();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout,"%10.1lf",Prob.Zopt);
+		fprintf(fout,"%7.2lf",Prob.Gap);
+		fprintf(fout,"%8d",Prob.Nodes);
+		fprintf(fout,"%8d",Prob.Cuts);
+		fprintf(fout,"%8.2lf",dt);
+		fflush(fout);
+
+		// Cover
+		Prob.cfg.TimeLimit=TLimMIPb;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.Pred=0;
+		Prob.cfg.Cover=1;
+		Prob.cfg.Lifting=1;
+		Prob.cfg.AddLB=0;
+
+		t1 = clock();
+		Prob.Optimize();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout,"%10.1lf",Prob.Zopt);
+		fprintf(fout,"%7.2lf",Prob.Gap);
+		fprintf(fout,"%8d",Prob.Nodes);
+		fprintf(fout,"%8d",Prob.Cuts);
+		fprintf(fout,"%8.2lf",dt);
+		fflush(fout);
+
+		// Lifted Cover
+		Prob.cfg.TimeLimit=TLimMIPb;
+		Prob.cfg.Sentinel=0;
+		Prob.cfg.Pred=1;
+		Prob.cfg.Cover=1;
+		Prob.cfg.Lifting=1;
+		Prob.cfg.AddLB=0;
+
+		t1 = clock();
+		Prob.Optimize();
+		t2 = clock();
+		dt = (double)(t2-t1)/CLOCKS_PER_SEC;
+
+		fprintf(fout,"%10.1lf",Prob.Zopt);
+		fprintf(fout,"%7.2lf",Prob.Gap);
+		fprintf(fout,"%8d",Prob.Nodes);
+		fprintf(fout,"%8d",Prob.Cuts);
+		fprintf(fout,"%8.2lf",dt);
+		fflush(fout);
+
+		fprintf(fout,"\n");
+
+	}
+
+	fclose(fpro);
+	fclose(fout);
+	fclose(fout1);
+	fclose(fout2);
+}
+
+long MangiaPath(char *fname)
+{
+	long i,j;
+
+	j=0;
+	for (i=0; fname[i]!='\0'; i++)
+		if (fname[i]==92)
+			j=i+1;
+
+	return j;
+}
